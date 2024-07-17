c68eb8d5189dc38250c16d1b6b9ffd841c1165c7
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-07-17 11:28:57 +0530

    Day-05 code added

---
+++ Day-05_Functions/01. Functions.js
@@ -0,0 +1,119 @@
+// Activity 1: Function Declaration ✅
+// - Task 1: Write a function to check if a number is even or odd and log the result to the console.
+function isEvenOrOdd(num) {
+  if (num % 2 === 0) {
+    console.log(`${num} is a Even Number`);
+  } else {
+    console.log(`${num} is a Odd Number`);
+  }
+}
+isEvenOrOdd(4);
+isEvenOrOdd(7);
+
+console.log("Task 1 Ended Here!✅");
+
+// - Task 2: Write a function to calculate the square of a number and return the result.
+function squareCalculator(a) {
+  return a ** 2;
+}
+console.log(squareCalculator(5));
+console.log(squareCalculator(7));
+
+console.log("Task 2 Ended Here!✅");
+
+// ********************************* //
+
+// Activity 2: Function Expression ✅
+// - Task 3: Write a function expression to find the maximum of two numbers and log the result to the console.
+const maxNum = function (num1, num2) {
+  if (num1 > num2) {
+    console.log(num1 + " is Greater.");
+  } else {
+    console.log(num2 + " is Greater.");
+  }
+};
+maxNum(7, 24);
+maxNum(28, 25);
+
+console.log("Task 3 Ended Here!✅");
+
+// - Task 4: Write a function expression to concatenate two strings and return the result.
+const concatenateStr = function (str1, str2) {
+  return str1 + str2;
+};
+console.log(concatenateStr("Hello, ", "ChaiAurCode !"));
+console.log(concatenateStr("Chai ", "Aur JavaScript!"));
+
+console.log("Task 4 Ended Here!✅");
+
+// ********************************* //
+
+// Activity 3: Arrow Functions ✅
+// - Task 5: Write an arrow function to calculate the sum of two numbers and return the result.
+const sum = (num1, num2) => num1 + num2;
+console.log(sum(2, 3));
+console.log(sum(10, 20));
+
+console.log("Task 5 Ended Here!✅");
+
+// - Task 6: Write an arrow function to check if a string contains a specific character and return a boolean value.
+const charCheck = (str, char) => str.includes(char);
+console.log(charCheck("Chai Aur Code", "a"));
+console.log(charCheck("JavaScript", "1"));
+
+console.log("Task 6 Ended Here!✅");
+
+// ********************************* //
+
+// Activity 4: Function Parameters and Default Values ✅
+// - Task 7: Write a function that takes two parameters and returns their product. Provide a default value for the second parameter.
+function multiplication(a, b = 1) {
+  return a * b;
+}
+console.log(multiplication(5, 2));
+console.log(multiplication(7));
+
+console.log("Task 7 Ended Here!✅");
+
+// - Task 8: Write a function that takes a person's name and age and returns a greeting message. Provide a default value for the age.
+function greet(name, age = "undefined") {
+  return `Good Morning ! Hello, ${name} having age of ${age} years old.`;
+}
+console.log(greet("John", 30));
+console.log(greet("Chinmay"));
+
+console.log("Task 8 Ended Here!✅");
+
+// ********************************* //
+
+// Activity 5: Higher-Order Functions ✅
+// - Task 9: Write a higher-order function that takes a function and a number, and calls the function that many times.
+function repeatFnc(fn, times) {
+  for (let i = 0; i < times; i++) {
+    fn();
+  }
+}
+function greet() {
+  console.log("Hello! ChaiAurCode");
+}
+repeatFnc(greet, 3);
+
+console.log("Task 9 Ended Here!✅");
+
+// - Task 10: Write a higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result.
+function compose(fn1, fn2) {
+  return function (val) {
+    return fn2(fn1(val));
+  };
+}
+function double(num) {
+  return num * 2;
+}
+function square(num) {
+  return num ** 2;
+}
+const doubleThenSquare = compose(double, square);
+console.log(doubleThenSquare(2));
+console.log(doubleThenSquare(3));
+
+console.log("Task 10 Ended Here!✅");
---
+++ Day-05_Functions/README.md
@@ -0,0 +1,61 @@
+# Day-05 : Functions 🍵❤️‍🔥
+
+![Day-05 Code Snap](Day-05%20Code%20Snap.png)
+
+## Tasks | Activities 🌟
+
+_**Activity 1: Function Declaration**_
+
+- **Task 1:** Write a function to check if a number is even or odd and log the result to the console.
+- **Task 2:** Write a function to calculate the square of a number and return the result.
+
+<hr/>
+
+_**Activity 2: Function Expression**_
+
+- **Task 3:** Write a function expression to find the maximum of two numbers and log the result to the console.
+- **Task 4:** Write a function expression to concatenate two strings and return the result.
+
+<hr/>
+
+_**Activity 3: Arrow Functions**_
+
+- **Task 5:** Write an arrow function to calculate the sum of two numbers and return the result.
+- **Task 6:** Write an arrow function to check if a string contains a specific character and return a boolean value.
+
+<hr/>
+
+_**Activity 4: Function Parameters and Default Values**_
+
+- **Task 7:** Write a function that takes two parameters and returns their product. Provide a default value for the second parameter.
+- **Task 8:** Write a function that takes a person's name and age and returns a greeting message. Provide a default value for the age.
+
+<hr/>
+
+_**Activity 5: Higher-Order Functions**_
+
+- **Task 9:** Write a higher-order function that takes a function and a number, and calls the function that many times.
+- **Task 10:** Write a higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result.
+
+<hr/>
+
+### Feature Request 🙇‍♂️
+
+1. **Even or Odd Function Script:** Write a script that includes a function to check if a number is even or odd and logs the result.
+
+2. **Square Calculation Function Script:** Create a script that includes a function to calculate the square of a number and returns the result.
+
+3. **Concatenation Function Script:** Write a script that includes al function expression to concatenate two strings and returns the result.
+
+4. **Sum Calculation Arrow Function Script:** Create a script that includes an arrow function to calculate the sum of two numbers and returns the result.
+
+5. **Higher-Order Function Script:** Write a script that includes a higher-order function to apply a given function multiple times.
+
+### Achievement 🏆
+
+By the end of these activities, you will:
+
+- Understand and define functions using function declarations, expressions, and arrow functions.
+- Use function parameters and default values effectively.
+- Create and utilize higher-order functions.
+- Apply functions to solve common problems and perform calculations. Enhance code reusability and organization using functions.
